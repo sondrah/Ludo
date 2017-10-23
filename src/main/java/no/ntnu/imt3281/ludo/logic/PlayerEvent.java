968a0b4babf8a685e5339d718cee10e069ac4483
Solex95 <sondreahlgren@gmail.com>
2017-10-23 18:39:37 +0200

    startet lett på playerevent på trooss av at jeg ikke skulle gjøre det

--- src/main/java/no/ntnu/imt3281/ludo/logic/PlayerEvent.java
+++ src/main/java/no/ntnu/imt3281/ludo/logic/PlayerEvent.java
@@ -1,5 +1,31 @@
 package no.ntnu.imt3281.ludo.logic;
 
-public class PlayerEvent {
 
+
+/**
+ * 
+ * @author sondrah
+ *
+ */
+public class PlayerEvent extends java.util.EventObject {
+
+	
+	private static final int PLAYING;
+	private static final int WAITING;
+	private static final int LEFTGAME;
+	private static final int WON;
+	private int activePlayer;
+	private int state;
+	
+	
+	public PlayerEvent(Object source) 
+	{
+		super(source);
+		
+	}
+	
+	
+	
+	
+	
 }
