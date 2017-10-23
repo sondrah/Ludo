438b5b925fa888b84dde9760ae34133e16c81cd8
Bjørn Ole Myrold <bjornomy@stud.ntnu.no>
2017-10-23 13:03:48 +0200

    Masse ludo stuffs

--- src/main/java/no/ntnu/imt3281/ludo/logic/Ludo.java
+++ src/main/java/no/ntnu/imt3281/ludo/logic/Ludo.java
@@ -54,7 +54,9 @@ public class Ludo {
 		
 		if(players.size() < 2){
 			players.clear();
-			throw new NotEnoughPlayersException();
+			throw new NotEnoughPlayersException(
+					  "Ludo#Ludo(String, String, String, String):"
+					+ "The number of players must be more than 2");
 		}
 		else{
 			// TODO: her må vi gjøre resten av initen
@@ -125,7 +127,9 @@ public class Ludo {
 		// om vi trenger mer funksjonalitet her
 		for(String p : players){
 			if(p == player){
-				throw new IllegalPlayerNameException();
+				throw new IllegalPlayerNameException(
+						  "Ludo#addPlayer(String): Can't have the name: "
+						+ player);
 			}
 		}
 	}
@@ -136,7 +140,21 @@ public class Ludo {
 	 * @throws NoSuchPlayerException
 	 */
 	public void removePlayer(String player) throws NoSuchPlayerException{
+		boolean found = false;
+		int i = 0;
 		
+		while(!found && i < players.size()) {
+			if(players.get(i++) == player) {
+				players.remove(i);
+				found = true;
+			}
+		}
+		
+		if(!found) {
+			throw new NoSuchPlayerException(
+					  "Ludo#removePlayer(String):"
+					+ "The player" + player + " was not found"); 
+		}
 	}
 	
 	
@@ -148,9 +166,127 @@ public class Ludo {
 	 * @return the position, relative to the player, as int
 	 */
 	public int getPosition(int player, int piece){
+		// TODO: FIXME
 		return 0;
 	}
 	
 	
+	/**
+	 * Gets the current active player (RED, GREEN, YELLOW, BLUE) 
+	 * @return the current active player
+	 */
+	public int activePlayer() {
+		return this.activePlayer;
+	}
 	
+	/**
+	 * Throws a D6
+	 * @return value between 1 and 6 (inclusive)
+	 */
+	public int throwDice() {
+		int dice = 0;
+		randomGenerator = new Random();
+		
+		dice = randomGenerator.nextInt(5) + 1;
+		alertThrowDice(dice);
+		return dice;
+	}
+	
+	/**
+	 * Clientside dicethrow, returns the given value
+	 * @param value the value the throw should result in
+	 * @return the given value
+	 */
+	public int throwDice(int value) {
+		alertThrowDice(value);
+		return value;
+	}
+	
+	
+	/**
+	 * sends the DiceEvent to all registered DiceListners
+	 * @param value of the dicethrow
+	 */
+	private void alertThrowDice(int value) {
+		for(DiceListner dl : diceListners) {
+			dl.diceThrown(new DiceEvent("Server", value, activePlayer));
+		}
+	}
+	
+	/**
+	 * Handles all movements of pieces in the game 
+	 * @param player the player whose piece we want to move 
+	 * @param from position to move from (relative to the player)
+	 * @param to position to move from (relative to he player)
+	 * 
+	 * @return true if the piece could move, false otherwise
+	 */
+	private boolean movePiece(int player, int from, int to) {
+		// FIXME
+		if (from == 0) {
+			if(dice == 6) {
+				playerPieces[player][0] = to;
+			}
+		}
+	}
+	
+	/**
+	 * Gets the current state of the game
+	 * @return gamestate
+	 */
+	public String getStatus() {
+		if(activePlayers() == 0) {
+		}
+		else if(activePlayers() >= 1){
+			if(dice == 0) {
+				return "Initiated";
+			}
+			else { 
+				return "Started";
+			}
+		}
+		else {
+			if(getWinner() > 0) {
+				return "Finished";
+			}
+		}
+	}
+	
+	
+	/**
+	 * Checks if someone has won the game
+	 * @return the winner of the game
+	 */
+	public int getWinner() { 
+		for(int i = 0; i < players.size(); i++) {
+			for(int j = 0; j < 4; j++) {
+				// TODO: check if 59 is correct pos
+				if(playerPieces[i][j] == 59);
+			}
+		}
+	}
+	
+	/**
+	 * Adds a DiceListner to the game
+	 * @param diceListner to be added
+	 */
+	public void addDiceListner(DiceListner diceListner) {
+		diceListners.add(diceListner);
+	}
+	
+	/**
+	 * Adds PlayerListner to the game
+	 * @param playerListner to be added
+	 */
+	public void addPlayerListner(PlayerListner playerListner) {
+		playerListners.add(playerListner);
+	}
+	
+	/**
+	 * Adds a PieceListner to the game
+	 * @param pieceListner to be added
+	 */
+	public void addPieceListner(PieceListner pieceListner) {
+		pieceListeners.add(pieceListner);
+	}
 }
