08a346510f2d05f2fe3daad2190619daa4a0e189
Solex95 <sondreahlgren@gmail.com>
2017-10-25 14:06:21 +0200

    prøver å forstå exceptions

---
+++ src/main/java/no/ntnu/imt3281/ludo/logic/IllegalPlayerNameException.java
@@ -0,0 +1,10 @@
+package no.ntnu.imt3281.ludo.logic;
+
+public class IllegalPlayerNameException extends Ludo {
+	
+	public IllegalPlayerNameException(String txt){
+		
+	}
+	
+
+}
--- src/main/java/no/ntnu/imt3281/ludo/logic/NotEnoughPlayersException.java
+++ src/main/java/no/ntnu/imt3281/ludo/logic/NotEnoughPlayersException.java
@@ -5,14 +5,9 @@ package no.ntnu.imt3281.ludo.logic;
 public class NotEnoughPlayersException extends Exception {
 	
 	
-	public NotEnoughPlayersException(String txt) {
-											// What is going on here.... BRACKFAST?
-		System.err.printf(txt, NotEnoughPlayersException);
-		
-		System.out.printf("The number of players must be more than 2");
+	public NotEnoughPlayersException(String txt) {  // ikke sikker om det skal være sånn
+		System.out.printf(txt);
 	}
-	
-	
 	
 	
 	public String getMessage() {
@@ -22,3 +17,4 @@ public class NotEnoughPlayersException extends Exception {
 	
 
 }
+		
