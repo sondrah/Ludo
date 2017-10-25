67126eda2c3db897867c52f4f169870255306bb5
snorrerogne <snorre.r@hotmail.com>
2017-10-25 13:38:24 +0200

    playerEvent eskl. javaDoc

--- src/main/java/no/ntnu/imt3281/ludo/logic/PlayerEvent.java
+++ src/main/java/no/ntnu/imt3281/ludo/logic/PlayerEvent.java
@@ -1,27 +1,74 @@
 package no.ntnu.imt3281.ludo.logic;
 
 
-
 /**
- * 
- * @author sondrah
+ * @author Snorre
  *
  */
 public class PlayerEvent extends java.util.EventObject {
 
 	
-	private static final int PLAYING;
-	private static final int WAITING;
-	private static final int LEFTGAME;
-	private static final int WON;
+	public static final int PLAYING = 0;
+	public static final int WAITING = 1;
+	public static final int LEFTGAME = 2;
+	public static final int WON = 3;
 	private int activePlayer;
 	private int state;
 	
 	
-	public PlayerEvent(Object source) 
+	/**
+	 * @param obj
+	 */
+	public PlayerEvent(Object obj) 
 	{
-		super(source);
-		
+		super(obj);
+	}
+	
+	/**
+	 * @param obj
+	 * @param active
+	 * @param state
+	 */
+	public PlayerEvent(Object obj, int active, int state) {
+		super(obj);
+		setActivePlayer(active);
+		setState(state);
+	}
+	
+	/**
+	 * @param obj
+	 * @return
+	 */
+	public boolean Equals(Object obj) {
+		return true;						//FIXME
+	}
+
+	/**
+	 * @return
+	 */
+	public int getActivePlayer() {
+		return activePlayer;
+	}
+	
+	/**
+	 * @param active
+	 */
+	public void setActivePlayer(int active) {
+		this.activePlayer = active;
+	}
+	
+	/**
+	 * @return
+	 */
+	public int getState() {
+		return state;
+	}
+
+	/**
+	 * @param state
+	 */
+	public void setState(int state) {
+		this.state = state;
 	}
 	
 	
