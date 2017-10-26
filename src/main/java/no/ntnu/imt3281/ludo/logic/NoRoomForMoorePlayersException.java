b96ada0b35159a5fdb8e61ed88d9787a82fe0219
Solex95 <sondreahlgren@gmail.com>
2017-10-26 16:56:14 +0200

    liten fix på exception og Ludo constructor

--- src/main/java/no/ntnu/imt3281/ludo/logic/NoRoomForMoorePlayersException.java
+++
@@ -1,8 +0,0 @@
-package no.ntnu.imt3281.ludo.logic;
-
-public class NoRoomForMoorePlayersException extends java.lang.RuntimeException {
-
-	public NoRoomForMoorePlayersException(String txt) {
-		System.err.printf(txt);
-	}
-}
