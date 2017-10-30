7583e4151839130d4154e11d8cb330d437317954
Bjørn Ole Myrold <bjornomy@stud.ntnu.no>
2017-10-30 14:05:35 +0100

    Added super i exceptions

--- src/main/java/no/ntnu/imt3281/ludo/logic/IllegalPlayerNameException.java
+++ src/main/java/no/ntnu/imt3281/ludo/logic/IllegalPlayerNameException.java
@@ -3,6 +3,7 @@ package no.ntnu.imt3281.ludo.logic;
 public class IllegalPlayerNameException extends java.lang.RuntimeException {
 	
 	public IllegalPlayerNameException(String txt){
+		super(txt);
 		System.err.printf(txt);
 	}
 	
