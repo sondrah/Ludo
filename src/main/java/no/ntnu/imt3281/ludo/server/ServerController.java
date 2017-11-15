bfa1170b4a3f9286184679e078b2d29a967bcf3e
Solex95 <sondreahlgren@gmail.com>
2017-11-15 13:42:49 +0100

    sjekker DB ved start og lager en hvis det ikke finns

---
+++ src/main/java/no/ntnu/imt3281/ludo/server/ServerController.java
@@ -0,0 +1,63 @@
+package no.ntnu.imt3281.ludo.server;
+
+import java.sql.SQLException;
+
+/**
+ * Master-server who controls DB, chat & Game  
+ *
+ */
+public class ServerController {
+
+	/** The 'url' to our database (local) */
+	private String url = "jdbc:derby:BadgerDB;";
+	
+	
+	public static void main(String[] args) {
+		ServerController foo = new ServerController();
+	}
+	
+	
+	public ServerController() {		
+		
+		try {
+			Database db = new Database(url);	// tries to connect to DB	
+		} catch(SQLException sqle) {			
+			url += "create=true";				
+			sqle.printStackTrace();
+			try {
+				Database db = new Database(url);	// if DB not found, adds "create=true" to make DB
+			} catch(SQLException sqle2) {
+				System.err.println("No DB after 2nd try"); 
+				sqle2.printStackTrace();
+			}
+		}
+		
+		ChatServer cs = new ChatServer();	
+		
+		
+		
+		
+		
+		
+		/*
+		 Lage div chat
+		 funksjoner som interakterer med DB
+		 */
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
