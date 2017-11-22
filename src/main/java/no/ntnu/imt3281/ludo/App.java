fea082e5d0a4c44409a8f0c6aa4f16476873c329
Solex95 <sondreahlgren@gmail.com>
2017-11-22 11:25:38 +0100

    La til main

---
+++ src/main/java/no/ntnu/imt3281/ludo/App.java
@@ -0,0 +1,15 @@
+package no.ntnu.imt3281.ludo;
+
+import javafx.application.Application;
+import no.ntnu.imt3281.ludo.client.Client;
+import no.ntnu.imt3281.ludo.server.ServerController;
+
+public class App extends Application {
+	
+	public static void main(String[] args) {
+		ServerController servercontroller = new ServerController();
+		Client c = new Client();
+		launch(args);
+	}
+
+}
